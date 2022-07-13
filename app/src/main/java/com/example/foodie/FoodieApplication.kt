f76bb581246415ef4c5072380452416813c70c4c
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2022-07-13 18:26:32 +0300

    inject foodRecipesApi as a dependency into RemoteDataSource

--- app/src/main/java/com/example/foodie/FoodieApplication.kt
+++ app/src/main/java/com/example/foodie/FoodieApplication.kt
@@ -3,7 +3,8 @@ package com.example.foodie
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+// NEVER forget to add your application, activities, services and broadcastreceiver classes to the manifest, else they won't work ;)
 @HiltAndroidApp //you must annotation application class with this annotation to use Dagger-Hilt
-class FoodyApplication: Application() {
+class FoodieApplication: Application() {
 
 }
--- app/src/main/java/com/example/foodie/RemoteDataSource.kt
+++ app/src/main/java/com/example/foodie/RemoteDataSource.kt
@@ -1,4 +1,13 @@
 package com.example.foodie
 
-class RemoteDataSource() {
+import com.example.foodie.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+   private val foodRecipesApi: FoodRecipesApi
+) {
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe>{
+        return foodRecipesApi.getRecipes(queries)
+    }
 }
