c9a37064de8ba22aa5f44bbae957c117db43a171
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2022-07-15 01:36:05 +0300

    create entity (table) class

--- app/src/main/java/com/example/foodie/utils/Constants.kt
+++ app/src/main/java/com/example/foodie/utils/Constants.kt
@@ -3,9 +3,13 @@ package com.example.foodie
 // API constants
 const val BASE_URL = "https://api.spoonacular.com/"
 
-//@QueryMap keys
+// @QueryMap keys
 const val NUMBER_QUERY = "number"
 const val TYPE_QUERY = "type"
 const val DIET_QUERY = "diet"
 const val ADD_RECIPE_INFORMATION_QUERY = "addRecipeInformation"
 const val FILL_INGREDIENTS_QUERY = "fillIngredients"
+
+// Room database consts
+const val  FOODIE_DATABASE = "foodie_database.db" // it doesn't matter if your sqlite database file extension is .db, .db3, .sqlite, .sqlite3 or anything else or nothing at all, it's just a naming convention. Really down to personal preference.
+const val  RECIPES_TABLE =  "recipes_table"
